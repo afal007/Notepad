f18ba7ec90b9b7fad2e29d64324ede88052e20c5
Aleksander Fal <falalexandr007@gmail.com>
2017-08-05 16:27:19 +0700

    Added confirmation dialog
    Confirmation dialog for situations in which user haven't
    saved the changes and requests to close app or to create
    or open new document.

--- src/com/afal/notepad/NotepadController.java
+++ src/com/afal/notepad/NotepadController.java
@@ -2,18 +2,20 @@ package com.afal.notepad;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import javafx.event.ActionEvent;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 
 public class NotepadController implements Initializable {
     private boolean wasChanged = false;
     private Stage mainStage;
+    private static final int CANCEL = -1;
+    private static final int CONTINUE = 0;
 
     @FXML
     TextArea textArea;
@@ -33,18 +35,42 @@ public class NotepadController implements Initializable {
     @FXML
     public void handleNewActionEvent(ActionEvent e) {
         if(wasChanged)
-            showWasChangedDialog();
+            if(showConfirmationDialog() == CANCEL)
+                return;
 
         mainStage.setTitle(Main.MAIN_STAGE_TITLE);
         textArea.setText("");
         wasChanged = false;
     }
 
-    private void showWasChangedDialog() {
+    private int showConfirmationDialog() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Java Notepad");
+        alert.setHeaderText("Do you want to save changes?");
+
+        ButtonType save = new ButtonType("Save");
+        ButtonType dontSave = new ButtonType("Don't save");
+        ButtonType cancel = new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE);
+
+        alert.getButtonTypes().setAll(save, dontSave, cancel);
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (!result.isPresent() || result.get() == cancel) {
+            return CANCEL;
+        } else if(result.get() == save) {
+            //save
+        }
+
+        return CONTINUE;
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         mainStage = Main.getMainStage();
+        mainStage.setOnCloseRequest((e) -> {
+            if(wasChanged)
+                if(showConfirmationDialog() == CANCEL)
+                    e.consume(); //Cancel close request
+        });
     }
 }
