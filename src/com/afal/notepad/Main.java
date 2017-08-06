48b41dc4bdfcbed1ae7ce64d0ecdb374b3062400
Aleksander Fal <falalexandr007@gmail.com>
2017-08-06 15:13:51 +0700

    Added app icon

--- src/com/afal/notepad/Main.java
+++ src/com/afal/notepad/Main.java
@@ -4,23 +4,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     static final String APP_NAME = "Java Notepad";
     static final String NEW_FILE_NAME = "Untitled";
     static final String MAIN_STAGE_TITLE = NEW_FILE_NAME + " - " + APP_NAME;
+    private static final Image APP_ICON = new Image("file:res/icon.png");
 
     private static Stage mainStage;
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        mainStage = primaryStage;
-        Parent root = FXMLLoader.load(getClass().getResource("notepad.fxml"));
-        primaryStage.setTitle(MAIN_STAGE_TITLE);
-        primaryStage.setScene(new Scene(root, 700, 600));
-        primaryStage.show();
-    }
-
 
     public static void main(String[] args) {
         launch(args);
@@ -28,5 +21,15 @@ public class Main extends Application {
 
     static Stage getMainStage() {
         return mainStage;
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        mainStage = primaryStage;
+        Parent root = FXMLLoader.load(getClass().getResource("notepad.fxml"));
+        primaryStage.setTitle(MAIN_STAGE_TITLE);
+        primaryStage.setScene(new Scene(root, 700, 600));
+        primaryStage.getIcons().setAll(APP_ICON);
+        primaryStage.show();
     }
 }
