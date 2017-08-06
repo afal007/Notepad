a642972ae97fa66201f91d1c97b5996f6ff04d5a
Aleksander Fal <falalexandr007@gmail.com>
2017-08-06 12:20:15 +0700

    Added FileIO helper class
    Provides writeTo and readFrom file methods.

---
+++ src/com/afal/notepad/FileIO.java
@@ -0,0 +1,22 @@
+package com.afal.notepad;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class FileIO {
+    public static void writeTo(File file, String text) throws IOException {
+        try (FileWriter out = new FileWriter(file)) {
+            out.write(text);
+            out.flush();
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Couldn't write to specified file.", e);
+        }
+    }
+
+    public static String readFrom(File file) throws IOException {
+        Stream<String> lines = Files.lines(file.toPath());
+        return lines.collect(Collectors.joining(" "));
+    }
+}
