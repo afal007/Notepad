32962f1f4db86665ae2a31132c39957bfd42dc16
Aleksander Fal <falalexandr007@gmail.com>
2017-08-06 15:11:51 +0700

    Fixed file reading

--- src/com/afal/notepad/file/FileIO.java
+++ src/com/afal/notepad/file/FileIO.java
@@ -1,4 +1,4 @@
-package com.afal.notepad;
+package com.afal.notepad.file;
 
 import java.io.*;
 import java.nio.file.Files;
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class FileIO {
-    public static void writeTo(File file, String text) throws IOException {
+    public static void writeTo(File file, String text) throws IllegalArgumentException {
         try (FileWriter out = new FileWriter(file)) {
             out.write(text);
             out.flush();
@@ -17,6 +17,6 @@ public class FileIO {
 
     public static String readFrom(File file) throws IOException {
         Stream<String> lines = Files.lines(file.toPath());
-        return lines.collect(Collectors.joining(" "));
+        return lines.collect(Collectors.joining("\n"));
     }
 }
