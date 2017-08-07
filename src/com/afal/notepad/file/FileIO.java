9cbcab87d22357b4097139b3cdc59b3c8bcebcec
afal <falalexandr007@gmail.com>
2017-08-07 21:12:30 +0700

    Fixed line breaks issue

--- src/com/afal/notepad/file/FileIO.java
+++ src/com/afal/notepad/file/FileIO.java
@@ -7,9 +7,14 @@ import java.util.stream.Stream;
 
 public class FileIO {
     public static void writeTo(File file, String text) throws IllegalArgumentException {
-        try (FileWriter out = new FileWriter(file)) {
-            out.write(text);
-            out.flush();
+        try (BufferedReader reader = new BufferedReader(new StringReader(text));
+             PrintWriter writer = new PrintWriter(new FileWriter(file))) {
+            /*
+            * If we use simple FileWriter line breaks are not working for Windows, because from text area we always get
+            * \n character for line break. But in Windows \r\n sequence is used for line break. So we have to reread
+            * the text and using println insert platform-specific break line characters.
+            * */
+            reader.lines().forEach(writer::println);
         } catch (IOException e) {
             throw new IllegalArgumentException("Couldn't write to specified file.", e);
         }
